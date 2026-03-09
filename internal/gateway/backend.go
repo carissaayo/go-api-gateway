b93f7ed11dc3ee1c3e4d15d3190c6e510759c506
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-09 13:15:17 +0100

    no  need for the circuitbreaker middleware

--- internal/gateway/backend.go
+++ internal/gateway/backend.go
@@ -1,5 +1,7 @@
 package gateway
 
+import "github.com/carissaayo/go-api-gateway/internal/circuitbreaker"
+
 func (gw *Gateway) RegisterBackend(name string, url string, weight int) error {
-	return gw.proxy.AddBackend(name, url, weight)
+	return gw.proxy.AddBackend(name, url, weight, circuitbreaker.DefaultConfig())
 }
